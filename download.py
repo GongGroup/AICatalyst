b490d9cb9204fbb5970d57b140058e64ee635a84
Rasic2 <1051987201@qq.com>
2022-07-27 16:56:37 +0800

    add download.py (download literature from publication)

---
+++ download.py
@@ -0,0 +1,45 @@
+import hashlib
+import random
+import time
+from pathlib import Path
+
+import numpy as np
+from selenium import webdriver
+
+from logger import logger
+
+with open("datafile.1.csv", encoding='utf-8') as f:
+    content = f.readlines()
+
+driver = webdriver.Chrome()
+driver.set_window_position(1200, 10)
+
+for line in content[:5]:
+    url, doi = line.strip().split(",")[-2:]
+    md5_name = hashlib.md5(url.encode(encoding='utf-8')).hexdigest()
+    if Path(f"literature/{md5_name}.html").exists():
+        logger.info(f"{md5_name}.html exists, continue")
+        continue
+
+    new_url = "http://dx.doi.org/" + doi if doi else doi
+    driver.get(new_url)
+    time.sleep(random.random() * 3)
+
+    scrollHeight = driver.execute_script("var q=document.body.scrollHeight; return(q)")
+
+    # generate random scroll distance, total scroll_t times, sum is scrollHeight
+    scroll_t = random.randint(20, 40)
+    loop_height = np.random.dirichlet(np.ones(scroll_t)) * scrollHeight
+    for i, h in zip(range(scroll_t), loop_height):
+        driver.execute_script(f"window.scrollBy(0,{h})")
+        time.sleep(random.random())
+    driver.execute_script(f"window.scrollTo(0,{scrollHeight})")
+
+    # write to *.html files
+    with open(f"literature/{md5_name}.html", "w", encoding='utf-8') as f:
+        f.write(driver.page_source)
+    logger.info(f"{md5_name}.html download successful")
+    time.sleep(random.random() * 5)
+
+logger.info("All literatures download successful")
+driver.quit()
--- load_html.py
+++ load_html.py
@@ -1,4 +1,6 @@
 import csv
+import os
+import re
 from pathlib import Path
 
 from pyquery import PyQuery as pq
@@ -9,6 +11,7 @@ htmls = sorted(list(Path("htmls").rglob("*.html")), key=lambda x: int(x.stem.spl
 
 titles = []
 urls = []
+dois = []
 
 for html_file in htmls:
     page = html_file.name.split(".")[0].split("_")[1]
@@ -22,17 +25,22 @@ for html_file in htmls:
     for record in records:
         title = " ".join(record('app-summary-title a').text().split())
         url = record('app-summary-record-links a').attr("href")
+        doi = re.findall("KeyAID=(.*)&DestApp=DOI", url) if url is not None else None
+        doi = doi[0] if isinstance(doi, list) and len(doi) else None
         titles.append(title)
         urls.append(url)
+        dois.append(doi)
         count += 1
     logger.info(f"Page {page}, record: {count}")
 
 logger.info(f"Total titles: {len(titles)} urls: {len(urls)}")
 
-data = list(zip(titles, urls))
+data = list(zip(titles, urls, dois))
 
-with open("datafile.csv", "w", encoding="utf-8") as f:
-    head = ["title", 'url']
+with open("datafile.csv", "w", encoding="utf-8", newline="") as f:
+    head = ["title", 'url', 'doi']
     writer = csv.writer(f)
     writer.writerow(head)
     writer.writerows(data)
+
+os.system("bash paper.sh datafile.csv> datafile.1.csv")
