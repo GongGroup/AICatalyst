5680c14b09b3bfde6060ac158d8d22641b6020c4
Rasic2 <1051987201@qq.com>
2022-07-28 21:34:13 +0800

    set page_load_strategy=none to accerlate && add retry

--- download.py
+++ download.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import numpy as np
 from selenium import webdriver
 import undetected_chromedriver as uc
+from selenium.common import JavascriptException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
@@ -13,17 +14,18 @@ from selenium.webdriver.support.wait import WebDriverWait
 from logger import logger
 
 if __name__ == '__main__':
+
     with open("datafile.1.csv", encoding='utf-8') as f:
         content = f.readlines()
 
     options = uc.ChromeOptions()
     options.add_argument(r"user-data-dir=C:\Users\hui_zhou\AppData\Local\Google\Chrome\User Data")
-
+    options.page_load_strategy = 'none'
     driver = uc.Chrome(options=options)
     driver.set_window_position(1200, 10)
     wait = WebDriverWait(driver, 60)
 
-    for line in content[:20]:
+    for line in content[:45]:
         url, doi = line.strip().split(",")[-2:]
         md5_name = hashlib.md5(url.encode(encoding='utf-8')).hexdigest()
         if Path(f"literature/{md5_name}.html").exists():
@@ -32,13 +34,21 @@ if __name__ == '__main__':
         new_url = "http://dx.doi.org/" + doi if doi else url
 
         driver.get(new_url)
-        time.sleep(random.random() * 3)
-        if url.split("=")[-1] == "Elsevier":
-            time.sleep(random.random() * 30)
-            wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#gh-branding > svg')))
-            time.sleep(random.random() * 3)
-
-        scrollHeight = driver.execute_script("var q=document.body.scrollHeight; return(q)")
+        while True:
+            count = 1
+            time.sleep(10)
+            if url.split("=")[-1] == "Elsevier":
+                time.sleep(random.random() * 30)
+                wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#gh-branding > svg')))
+                time.sleep(random.random() * 3)
+            try:
+                scrollHeight = driver.execute_script("var q=document.body.scrollHeight; return(q)")
+            except JavascriptException:
+                logger.info(f"Loading html failed, retry {count}")
+                count += 1
+                continue
+            else:
+                break
 
         # generate random scroll distance, total scroll_t times, sum is scrollHeight
         scroll_t = random.randint(20, 40)
--- download_check.py
+++ download_check.py
@@ -1,16 +1,33 @@
+import json
+import os
 import re
 from pathlib import Path
+
 from pyquery import PyQuery as pq
 
 from logger import logger
 
-literatures = Path("literature").rglob("*.html")
+# load excluded
+with open("exclude", "r", encoding="utf-8") as f:
+    exclude = [item.split(",")[0] for item in f.readlines()]
 
+# search which html failed
+failed = []
+literatures = Path("literature").rglob("*.html")
 logger.info(f"Check download literature completeness")
 for literature in literatures:
     with open(literature, "r", encoding="utf-8") as f:
         html = f.read()
     doc = pq(html)
-    conclusion = re.findall("[Cc]onclusion|[Ss]ummary", doc.text())
-    if not len(conclusion):
-        logger.info(f"{literature}: Search keyword `conclusion` failed, please check")
+    conclusion = re.findall("[Cc]onclusion|[Ss]ummary|[Oo]utlook", doc.text())
+    if not len(conclusion) and literature.stem not in exclude:
+        logger.info(f"{literature}: Search keyword `conclusion` failed, prepare check")
+        failed.append(literature.stem)
+
+# load md5_dict
+with open("md5_name.json", "r", encoding="utf-8") as f:
+    md5_dict = json.load(f)
+
+# locate which url failed
+for name in failed:
+    logger.info(md5_dict[name.strip()])
