9e36aecbcab5aac634bac08b7d9b3a54ed6c766c
Rasic2 <1051987201@qq.com>
2022-09-17 16:58:14 +0800

    add MCenter && Ligand in Molecule

--- calculator/molecule.py
+++ calculator/molecule.py
@@ -1,45 +1,60 @@
-import subprocess
-from typing import Optional
-
-from calculator.base import Atom, Atoms
 from calculator.rbase import RMolecule
 from common.fio import JsonIO
+from common.logger import logger
 
 
-class Ligand(object):
+class Ligand(RMolecule):
     _LigandInfo = JsonIO.read("ligand.json")
 
-    def __init__(self, name=None, atoms: Optional[Atoms] = None, smiles=None):
+    def __init__(self, name=None, smiles=None, rmol=None):
+        super(Ligand, self).__init__(rmol)
         self.name = name
-        self.atoms = atoms
         self.smiles = smiles
 
     def __repr__(self):
         return f"<{self.__class__.__name__} : {self.name}>"
 
-    @property
-    def rdkit_mol(self):
-        if self.smiles is not None:
-            return RMolecule.from_smiles(self.smiles)
-        return None
-
     @staticmethod
-    def from_strings(symbol, name=None):
-        smiles = Ligand._LigandInfo[symbol]
-        process = subprocess.Popen(f"obabel -:{smiles} --gen3d -oxyz", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        content = process.stdout.read().decode(encoding='utf-8')
-        lines = content.splitlines()
-
-        atoms = []
-        for line in lines:
-            items = line.split()
-            if len(items) == 4:
-                coords = list(map(float, items[1:]))
-                atoms.append(Atom(items[0], cart_coord=coords))
-        atoms = Atoms.from_list(atoms)
+    def from_strings(symbol, name=None, addH=True):
         if name is None:
             name = symbol
-        return Ligand(name, atoms, smiles)
+
+        smiles = Ligand._LigandInfo[symbol]
+        rmol = RMolecule.from_smiles(smiles, addH)
+
+        return Ligand(name, smiles, rmol)
+
+    def get_unsaturated_atoms(self):
+        unsaturated_atoms = []
+        for atom in self.atoms:
+            if atom.is_unsaturated:
+                unsaturated_atoms.append(atom)
+
+        if len(unsaturated_atoms) > 1:
+            logger.warning(f"Unsaturated atoms more than 1, may failed")
+        elif not len(unsaturated_atoms):
+            logger.warning(f"Unsaturated atoms equal to 0, may be a saturated molecule")
+
+        return unsaturated_atoms
+
+
+class MCenter(RMolecule):
+    def __init__(self, name=None, rmol=None):
+        super(MCenter, self).__init__(rmol)
+        self.name = name
+
+    @staticmethod
+    def from_strings(symbol, name=None, addH=False):
+        if name is None:
+            name = symbol
+
+        smiles = symbol
+        if "[" not in symbol:
+            smiles = f"[{symbol}]"
+
+        rmol = RMolecule.from_smiles(smiles, addH)
+
+        return MCenter(name, rmol)
 
 
 class Molecule(object):
@@ -47,14 +62,20 @@ class Molecule(object):
         self.center = center
         self.ligands = ligands
 
+        self.rearrange()
+
+    def __repr__(self):
+        return f"{self.center}{self.ligands}"
+
+    def rearrange(self):
+        angle = 360.0 / len(self.ligands)
+        print()
+
 
 if __name__ == '__main__':
-    # LigandInfo = JsonIO.read("ligand.json")
-    # smiles = LigandInfo['OAc']
-    # process = subprocess.Popen(f"obabel -:{smiles} --gen3d -oxyz", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    # content = process.stdout.read().decode(encoding='utf-8')
     OAc = Ligand.from_strings("OAc")
-    mol = Molecule("Pd", [OAc] * 2)
-    # lines = content.splitlines()
-    # print(lines)
+    Pd = MCenter.from_strings("Pd")
+    uatoms = OAc.get_unsaturated_atoms()
+    mol = Molecule(Pd, [OAc] * 2)
+
     print()
--- calculator/rbase.py
+++ calculator/rbase.py
@@ -1,79 +1,125 @@
+from pathlib import Path
+
+import yaml
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+yaml.warnings({'YAMLLoadWarning': False})
+
 
 class RAtom(object):
+    _element_file = Path(f"element.yaml")
+    _load = False
+    _elements = None
 
-    def __init__(self, atom):
-        self._atom = atom
+    def __new__(cls, *args, **kwargs):
+        cls.__load_config()
+        return object.__new__(cls)
+
+    def __init__(self, ratom, rposition):
+        self._ratom = ratom
+        self._rposition = rposition
 
     def __repr__(self):
-        return f"<{self.__class__.__name__} : {self.symbol}{self.total_degree}>"
+        return f"<{self.__class__.__name__} : {self.symbol}{self.total_valence} : {self.position}>"
+
+    @classmethod
+    def __load_config(cls):
+        """load element.yaml file (private classmethod)"""
+        if not cls._load:
+            with open(cls._element_file) as f:
+                cfg = f.read()
+            cls._elements = yaml.safe_load(cfg)
+            cls._load = True
+
+    @property
+    def is_unsaturated(self):
+        if self.total_valence < RAtom._elements[f'Element {self.symbol}']['valence']:
+            return True
+        return False
 
     @property
     def symbol(self):
-        return self._atom.GetSymbol()
+        return self._ratom.GetSymbol()
+
+    @property
+    def position(self):
+        return self._rposition.x, self._rposition.y, self._rposition.z
 
     @property
     def atom_map_num(self):
-        return self._atom.GetAtomMapNum()
+        return self._ratom.GetAtomMapNum()
 
     @property
     def atomic_num(self):
-        return self._atom.GetAtomicNum()
+        return self._ratom.GetAtomicNum()
 
     @property
     def formal_charge(self):
-        return self._atom.GetFormalCharge()
+        return self._ratom.GetFormalCharge()
 
     @property
-    def bonds(self):
-        return self._atom.GetBonds()
+    def _bonds(self):
+        return self._ratom.GetBonds()
 
     @property
     def degree(self):
-        return self._atom.GetDegree()
+        return self._ratom.GetDegree()
 
     @property
     def total_degree(self):
-        return self._atom.GetTotalDegree()
+        return self._ratom.GetTotalDegree()
+
+    @property
+    def hybridization(self):
+        return self._ratom.GetHybridization()
+
+    @property
+    def explicit_valence(self):
+        return self._ratom.GetExplicitValence()
+
+    @property
+    def implicit_valence(self):
+        return self._ratom.GetImplicitValence()
 
     @property
     def total_valence(self):
-        return self._atom.GetTotalValence()
+        return self._ratom.GetTotalValence()
 
 
 class RMolecule(object):
 
-    def __init__(self, mol):
-        self._mol = mol
+    def __init__(self, rmol):
+        self._rmol = rmol
 
     def __repr__(self):
-        return f"<{self.__class__.__name__} : {self.smiles}>"
+        return f"<{self.__class__.__name__} : {self.rsmiles}>"
 
     @property
-    def smiles(self):
-        return Chem.MolToSmiles(self._mol)
+    def rsmiles(self):
+        return Chem.MolToSmiles(self._rmol)
 
     @property
     def atoms(self):
-        atoms = []
-        for atom in self._mol.GetAtoms():
-            atoms.append(RAtom(atom))
-        return atoms
+        return [RAtom(atom, self._rmol.GetConformer().GetAtomPosition(index)) for index, atom in
+                enumerate(self._rmol.GetAtoms())]
 
     @property
     def num_atoms(self):
-        return self._mol.GetNumAtoms()
+        return self._rmol.GetNumAtoms()
 
     @staticmethod
-    def from_smiles(smiles):
-        mol = Chem.MolFromSmiles(smiles)
-        mol = AllChem.AddHs(mol)
-        return RMolecule(mol)
+    def from_smiles(smiles, addH=True):
+        rmol = Chem.MolFromSmiles(smiles)
+        if addH:
+            rmol = AllChem.AddHs(rmol)
+        AllChem.EmbedMolecule(rmol)
+        AllChem.MMFFOptimizeMolecule(rmol)
+        return rmol
 
 
 if __name__ == '__main__':
     smiles = "CC([O-])=O"
-    mol = RMolecule.from_smiles(smiles)
+    rmol = RMolecule.from_smiles(smiles)
+    rmol = RMolecule(rmol)
     print()
